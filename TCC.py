fc6bcc4a20f4d21fc081566305de924d865ca7a5
Eliel <eliel.silva@minervafoods.com>
2023-05-04 10:30:58 -0300

    .

--- TCC.py
+++ TCC.py
@@ -28,13 +28,13 @@ ef = 0.2
 N = list(range(0, orig))
 
 # Conjunto de pontos ferroviários (K)
-K = list(range(0, trans))
+K = [(i + orig) for i in range(trans)]
 
 # Conjunto de portos de navios destinos (M)
-M = list(range(0, port))
+M = [(i + orig + trans) for i in range(port)]
 
 # Conjunto de clientes (O)
-O = list(range(0, cli))
+O = [(i + orig + trans + port) for i in range(cli)]
 
 demanda = {}
 for o in O:
@@ -49,52 +49,48 @@ for k in K:
     CF[k] = sum(demanda.values()) if random.random() <= 0.3 else random.randint(500, 5000)
 
 CP = {}
-CP[0] = 300
-CP[1] = 150
-CP[2] = 100
-CP[3] = 10
-# for j in M:
-#     CP[j] = sum(demanda.values()) if random.random() <= 0.3 else random.randint(1000, 5000)
+for j in M:
+    CP[j] = sum(demanda.values()) if random.random() <= 0.3 else random.randint(1000, 5000)
 
-# points_orig = gen_points.get_point(lim_x_left=-100, lim_x_right=100,
-#                                    lim_y_down=-100, lim_y_up=100, n=orig)
-# points_trans = gen_points.get_point(lim_x_left=-250, lim_x_right=250,
-#                                     lim_y_down=-250, lim_y_up=250, n=trans)
-# points_portos = gen_points.get_point(lim_x_left=-1000, lim_x_right=1000,
-#                                      lim_y_down=-1000, lim_y_up=1000, n=port)
+points_orig = gen_points.get_point(lim_x_left=-100, lim_x_right=100,
+                                   lim_y_down=-100, lim_y_up=100, n=orig)
+points_trans = gen_points.get_point(lim_x_left=-250, lim_x_right=250,
+                                    lim_y_down=-250, lim_y_up=250, n=trans)
+points_portos = gen_points.get_point(lim_x_left=-1000, lim_x_right=1000,
+                                     lim_y_down=-1000, lim_y_up=1000, n=port)
 
-print("Pontos de origem")
-points_orig = []
-for _ in N:
-    points = input("Ponto X e Y: ").split(" ")
-    points_orig.append(gen_points.Point(int(points[0]), int(points[1])))
+# print("Pontos de origem")
+# points_orig = []
+# for _ in N:
+#     points = input("Ponto X e Y: ").split(" ")
+#     points_orig.append(gen_points.Point(int(points[0]), int(points[1])))
     
-print("Pontos de transbordo")
-points_trans = []
-for _ in K:
-    points = input("Ponto X e Y: ").split(" ")
-    points_trans.append(gen_points.Point(int(points[0]), int(points[1])))
+# print("Pontos de transbordo")
+# points_trans = []
+# for _ in K:
+#     points = input("Ponto X e Y: ").split(" ")
+#     points_trans.append(gen_points.Point(int(points[0]), int(points[1])))
     
-print("Pontos de porto")
-points_portos = []
-for _ in M:
-    points = input("Ponto X e Y: ").split(" ")
-    points_portos.append(gen_points.Point(int(points[0]), int(points[1])))
+# print("Pontos de porto")
+# points_portos = []
+# for _ in M:
+#     points = input("Ponto X e Y: ").split(" ")
+#     points_portos.append(gen_points.Point(int(points[0]), int(points[1])))
 
 dist_orig_trans = {}
 for i in N:
     for k in K:
-        dist_orig_trans[i, k] = gen_points.distance(points_orig[i].x, points_orig[i].y, points_trans[k].x, points_trans[k].y)
+        dist_orig_trans[i, k] = gen_points.distance(points_orig[i].x, points_orig[i].y, points_trans[k - orig].x, points_trans[k - orig].y)
 
 dist_trans_porto = {}
 for k in K:
     for j in M:
-        dist_trans_porto[k, j] = gen_points.distance(points_trans[k].x, points_trans[k].y, points_portos[j].x, points_portos[j].y)
+        dist_trans_porto[k, j] = gen_points.distance(points_trans[k - orig].x, points_trans[k - orig].y, points_portos[j - orig - trans].x, points_portos[j - orig - trans].y)
         
 dist_orig_porto = {}
 for i in N:
     for j in M:
-        dist_orig_porto[i, j] = gen_points.distance(points_orig[i].x, points_orig[i].y, points_portos[j].x, points_portos[j].y)
+        dist_orig_porto[i, j] = gen_points.distance(points_orig[i].x, points_orig[i].y, points_portos[j - orig - trans].x, points_portos[j - orig - trans].y)
 
 # Criação do modelo
 m = gp.Model("Transporte com Transbordo")
@@ -114,9 +110,6 @@ for i in N:
         for o in O:
             X[i, j, o] = m.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, name="X_Prod{}_Porto{}_Cliente{}".format(i, j, o))
 
-# y1 = m.addVar(vtype=gp.GRB.BINARY, name="y1")
-# y2 = m.addVar(vtype=gp.GRB.BINARY, name="y2")
-
 # Definição da função objetivo
 # Função objetivo de custo do transporte
 f1 = gp.quicksum(cr * dist_orig_trans[i, k] * X[i, k, o] for i in N for k in K for o in O) + \
@@ -124,23 +117,20 @@ f1 = gp.quicksum(cr * dist_orig_trans[i, k] * X[i, k, o] for i in N for k in K f
      gp.quicksum(cr * dist_orig_porto[i, j] * X[i, j, o] for i in N for j in M for o in O)
          
 
-# # Função objetivo de minimização de emissão de CO2
-# f2 = gp.quicksum(er * dist_orig_trans[i, k] * X[i, k, o] for i in N for k in K for o in O) + \
-#      gp.quicksum(ef * dist_trans_porto[k, j] * X[k, j, o] for j in M for k in K for o in O) + \
-#      gp.quicksum(er * dist_orig_porto[i, j] * X[i, j, o] for i in N for j in M for o in O)
+# Função objetivo de minimização de emissão de CO2
+f2 = gp.quicksum(er * dist_orig_trans[i, k] * X[i, k, o] for i in N for k in K for o in O) + \
+     gp.quicksum(ef * dist_trans_porto[k, j] * X[k, j, o] for j in M for k in K for o in O) + \
+     gp.quicksum(er * dist_orig_porto[i, j] * X[i, j, o] for i in N for j in M for o in O)
 
-m.setObjective(f1, gp.GRB.MINIMIZE)
+# m.setObjective(f1, gp.GRB.MINIMIZE)
 
-# m.setObjectiveN(f1, 0, priority=2, name="Custo do transporte")
-# m.setObjectiveN(f2, 1, priority=1, name="Emissão do transporte")
+m.setObjectiveN(f1, 0, priority=2, name="Custo do transporte")
+m.setObjectiveN(f2, 1, priority=1, name="Emissão do transporte")
 
 # Oferta dos produtores:
 for i in N:
     m.addConstr(((gp.quicksum(X[i, k, o] for k in K for o in O)) + (gp.quicksum(X[i, j, o] for j in M for o in O))) <= oferta[i], "Oferta_Prod_{}".format(i))
     
-# for i in N:
-#     m.addConstr(gp.quicksum(X[i, j, o] for j in M for o in O) <= oferta[i], "Oferta_Prod_{}".format(i))
-
 # Demanda dos clientes:
 for o in O:
     m.addConstr(((gp.quicksum(X[i, j, o] for i in N for j in M)) + (gp.quicksum(X[k, j, o] for k in K for j in M))) == demanda[o], "Demanda_Cli_{}".format(o))
@@ -151,23 +141,11 @@ for k in K:
 
 # Capacidade dos portos de navio
 for j in M:
-    m.addConstr(((gp.quicksum(X[i, j, o] for j in M for o in O)) + (gp.quicksum(X[k, j, o] for k in K for o in O))) <= CP[j], "Cap_Porto_{}".format(j))
+    m.addConstr(((gp.quicksum(X[i, j, o] for i in N for o in O)) + (gp.quicksum(X[k, j, o] for k in K for o in O))) <= CP[j], "Cap_Porto_{}".format(j))
 
 # Igualdade das quantidades
 for k in K:
     m.addConstr((gp.quicksum(X[i, k, o] for i in N for o in O))  == gp.quicksum(X[k, j, o] for j in M for o in O), "Igualdade_{}".format(k))
-        
-# for o in O:
-#     for k in K:
-#         m.addConstr(gp.quicksum(X[i, k, o] for i in N) <= CF[k] * y1)
-        
-# for o in O:
-#     for k in K:
-#         m.addConstr(gp.quicksum(X[k, j, o] for j in M) <= CP[j] * y1)
-        
-# for o in O:
-#     for j in M:
-#         m.addConstr(gp.quicksum(X[i, j, o] for i in N) <= CP[j] * y2)
 
 
 # Atualização do modelo com novas variáveis
